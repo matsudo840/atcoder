ebb96779597895e4472ee600672a404da37853de
Ryo Hashiguchi <hashiguchi.ryo.0823@gmail.com>
2026-03-23 02:27:58 +0900

    abs

---
+++ abs/abc086c/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+
+// 型エイリアス（Pythonの int/float の感覚に近づける）
+using ll = long long;
+using mint = modint998244353;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main()
+{
+  // 入出力の高速化（Pythonの sys.stdin.readline 的なやつ）
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  // ここから記述
+
+  return 0;
+}

425d8ff7c621a0868dd8367b919e8663b35403c0
Ryo Hashiguchi <hashiguchi.ryo.0823@gmail.com>
2026-03-24 23:20:05 +0900

    a05

--- tessoku-book/a05/main.cpp
+++ tessoku-book/a05/main.cpp
@@ -11,9 +11,18 @@ using mint = modint998244353;
 
 int main()
 {
+    int N, K;
+    cin >> N >> K;
 
+    int count = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= N; j++)
+        {
+            if (1 <= K - i - j && K - i - j <= N)
+                count++;
+        }
+    }
 
-
-
-
+    cout << count << endl;
 }

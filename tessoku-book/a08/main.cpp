18c389b1f9998b3713374300746d6d3b977f1852
Ryo Hashiguchi <hashiguchi.ryo.0823@gmail.com>
2026-03-25 00:08:19 +0900

    a08

--- tessoku-book/a08/main.cpp
+++ tessoku-book/a08/main.cpp
@@ -11,9 +11,25 @@ using mint = modint998244353;
 
 int main()
 {
+    vector<vector<int>> data(1509, vector<int>(1509));
 
+    int H, W, X;
+    cin >> H >> W;
+    for (int i = 1; i <= H; i++)
+    {
+        for (int j = 1; j <= W; j++)
+        {
+            cin >> X;
+            data.at(i).at(j) = data.at(i - 1).at(j) + data.at(i).at(j - 1) - data.at(i - 1).at(j - 1) + X;
+        }
+    }
 
-
-
-
+    int Q;
+    cin >> Q;
+    int A, B, C, D;
+    rep(i, Q)
+    {
+        cin >> A >> B >> C >> D;
+        cout << data.at(C).at(D) - data.at(A - 1).at(D) - data.at(C).at(B - 1) + data.at(A - 1).at(B - 1) << endl;
+    }
 }

fa60bb1f5f5c61d8ca1f3fc4562ba069150edfc8
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-03-29 21:56:56 +0900

    a13

--- tessoku-book/a13/main.cpp
+++ tessoku-book/a13/main.cpp
@@ -12,8 +12,25 @@ using mint = modint998244353;
 int main()
 {
 
+    int N, K;
+    cin >> N >> K;
 
+    vector<int> A(N + 9);
+    for (int i = 1; i <= N; i++)
+        cin >> A.at(i);
 
+    int L = 1;
+    ll ans = 0;
 
+    for (int R = 2; R <= N; R++)
+    {
+        while (A.at(R) - A.at(L) > K && L < R - 1)
+            L++;
 
+        if (A.at(R) - A.at(L) <= K)
+        {
+            ans += R - L;
+        }
+    }
+    cout << ans << endl;
 }

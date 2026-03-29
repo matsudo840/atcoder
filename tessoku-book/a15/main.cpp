b1e7535a0b1943e32bc5a3a7963197fd67a20e7d
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-03-29 23:17:11 +0900

    a15

--- tessoku-book/a15/main.cpp
+++ tessoku-book/a15/main.cpp
@@ -11,9 +11,40 @@ using mint = modint998244353;
 
 int main()
 {
+    int N;
+    cin >> N;
 
+    vector<int> A(N);
+    vector<int> B(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A.at(i);
+        B.at(i) = A.at(i);
+    }
 
+    set<int> B_tmp(all(B));
+    vector<int> B_uniq(all(B_tmp));
+    int s = B_uniq.size() - 1;
 
+    for (int i = 0; i < N; i++)
+    {
+        int L = 0, R = s;
+        int M;
+        while (L <= R)
+        {
+            M = (L + R) / 2;
+            if (A.at(i) < B_uniq.at(M))
+                R = M - 1;
+            if (A.at(i) == B_uniq.at(M))
+                break;
+            if (A.at(i) > B_uniq.at(M))
+                L = M + 1;
+        }
 
-
+        cout << M + 1;
+        if (i < N - 1)
+            cout << " ";
+        else
+            cout << endl;
+    }
 }

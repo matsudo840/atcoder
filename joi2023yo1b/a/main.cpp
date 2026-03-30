0a57946bce9b5ae8b1b55e6f2c47d1f8cee3af40
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-03-31 02:28:25 +0900

    joi2023yo1b

---
+++ joi2023yo1b/a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+// 型エイリアス（Pythonの int/float の感覚に近づける）
+using ll = long long;
+using mint = modint998244353;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+
+int main()
+{
+    int X;
+    cin >> X;
+
+    cout << X * 24 << endl;
+}

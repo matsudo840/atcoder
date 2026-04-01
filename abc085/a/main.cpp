1c72f48cae578b019c38b0bcd7beeb91524838ae
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-04-01 22:27:32 +0900

    abc085a

---
+++ abc085/a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+// 型エイリアス（Pythonの int/float の感覚に近づける）
+using ll = long long;
+using mint = modint998244353;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+
+int main()
+{
+    string S;
+    cin >> S;
+    S.at(3) = '8';
+    cout << S << endl;
+}

7f188c4c2b17ff8e9c8f3ce41bcaee3abef72cbe
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-04-01 22:31:47 +0900

    abc256a

---
+++ abc256/a/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+// 型エイリアス（Pythonの int/float の感覚に近づける）
+using ll = long long;
+using mint = modint998244353;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+
+int main()
+{
+    ll N;
+    cin >> N;
+
+    int ans = 1;
+    rep(i, N) ans *= 2;
+    cout << ans << endl;
+}

4efbea787251ff72427cc69a3e8c3febbbe419b8
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-04-08 23:36:54 +0900

    M&A 004

---
+++ math-and-algorithm/004/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+// 型エイリアス（Pythonの int/float の感覚に近づける）
+using ll = long long;
+using mint = modint998244353;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+
+int main()
+{
+    int A1, A2, A3;
+    cin >> A1 >> A2 >> A3;
+
+    cout << A1 * A2 * A3 << endl;
+}

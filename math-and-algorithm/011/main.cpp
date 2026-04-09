12364b650555d106465f97f578b114ea69f89c5e
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-04-09 21:04:27 +0900

    M&A 011

--- math-and-algorithm/011/main.cpp
+++ math-and-algorithm/011/main.cpp
@@ -9,11 +9,33 @@ using mint = modint998244353;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
 
+bool isprime(int n)
+{
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
 int main()
 {
+    int N;
+    cin >> N;
 
+    vector<int> result;
+    for (int i = 2; i <= N; i++)
+    {
+        if (isprime(i))
+            result.push_back(i);
+    }
 
-
-
-
+    for (int i = 0; i < result.size(); i++)
+    {
+        if (i != 0)
+            cout << " ";
+        cout << result.at(i);
+    }
+    cout << endl;
 }

706dc3a918ca3046a309ec5b086fecaaffdd433c
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-04-16 01:29:26 +0900

    tessoku b02

--- tessoku-book/b02/main.cpp
+++ tessoku-book/b02/main.cpp
@@ -11,9 +11,16 @@ using mint = modint998244353;
 
 int main()
 {
+    int A, B;
+    cin >> A >> B;
 
-
-
-
-
+    for (int i = A; i <= B; i++)
+    {
+        if (100 % i == 0)
+        {
+            cout << "Yes" << endl;
+            return (0);
+        }
+    }
+    cout << "No" << endl;
 }

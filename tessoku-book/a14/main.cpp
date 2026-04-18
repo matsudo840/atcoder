ab1114ead70c10154b335a17daea39bebf436fe8
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-04-18 18:58:47 +0900

    tessoku a14

--- tessoku-book/a14/main.cpp
+++ tessoku-book/a14/main.cpp
@@ -11,9 +11,38 @@ using mint = modint998244353;
 
 int main()
 {
+    int N, K;
+    cin >> N >> K;
 
+    vector<int> A(N), B(N), C(N), D(N);
+    for (int i = 0; i < N; i++)
+        cin >> A.at(i);
+    for (int i = 0; i < N; i++)
+        cin >> B.at(i);
+    for (int i = 0; i < N; i++)
+        cin >> C.at(i);
+    for (int i = 0; i < N; i++)
+        cin >> D.at(i);
 
+    vector<int> AB, CD;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            AB.push_back(A.at(i) + B.at(j));
+            CD.push_back(C.at(i) + D.at(j));
+        }
+    }
 
+    sort(all(CD));
 
+    string ans = "No";
+    for (int i = 0; i < AB.size(); i++)
+    {
+        auto itr = lower_bound(CD.begin(), CD.end(), K - AB.at(i));
+        if (itr != CD.end() && AB.at(i) + *itr == K)
+            ans = "Yes";
+    }
 
+    cout << ans << endl;
 }

d28dfb2bd129c79e757c73233ff67454959353a5
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-04-18 16:36:45 +0900

    tessoku a12

--- tessoku-book/a12/main.cpp
+++ tessoku-book/a12/main.cpp
@@ -9,11 +9,40 @@ using mint = modint998244353;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
 
+ll N, K;
+vector<ll> A(10009);
+
+// 答えがx以下であればtrue, そうでなければfalseを返す
+bool check(ll x)
+{
+    ll ans = 0;
+    for (int i = 1; i <= N; i++)
+        ans += x / A.at(i);
+
+    if (ans >= K)
+        return true;
+    else
+        return false;
+}
+
 int main()
 {
+    cin >> N >> K;
 
+    for (int i = 1; i <= N; i++)
+        cin >> A.at(i);
 
+    ll left = 1;
+    ll right = 1'000'000'000;
 
+    while (left < right)
+    {
+        ll mid = (left + right) / 2;
+        if (check(mid))
+            right = mid;
+        else
+            left = mid + 1;
+    }
 
-
+    cout << right << endl;
 }

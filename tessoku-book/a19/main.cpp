50f9e4c744d361de3c788c99ccbdb5487dc0d720
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-04-20 23:28:36 +0900

    tessoku a19

--- tessoku-book/a19/main.cpp
+++ tessoku-book/a19/main.cpp
@@ -11,9 +11,25 @@ using mint = modint998244353;
 
 int main()
 {
+    int N, W;
+    cin >> N >> W;
 
+    vector<ll> w(N + 1), v(N + 1);
+    for (int i = 1; i <= N; i++)
+        cin >> w.at(i) >> v.at(i);
 
+    // dp.at(n).at(w) := 1~nのアイテムを使った時に実現できる、重さw以下の最大価値v
+    vector<vector<ll>> dp(N + 1, vector<ll>(W + 9));
 
-
-
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= W; j++)
+        {
+            if (j - w.at(i) >= 0)
+                dp.at(i).at(j) = max(dp.at(i - 1).at(j - w.at(i)) + v.at(i), dp.at(i - 1).at(j));
+            else
+                dp.at(i).at(j) = dp.at(i - 1).at(j);
+        }
+    }
+    cout << dp.at(N).at(W) << endl;
 }

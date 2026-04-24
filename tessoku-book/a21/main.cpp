6327e610a55322acea68af3901a779175e1d5a91
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-04-25 01:27:03 +0900

    tessoku a21

--- tessoku-book/a21/main.cpp
+++ tessoku-book/a21/main.cpp
@@ -11,9 +11,37 @@ using mint = modint998244353;
 
 int main()
 {
+    int N;
+    cin >> N;
 
+    vector<int> P(N + 9), A(N + 9);
+    for (int i = 1; i <= N; i++)
+        cin >> P.at(i) >> A.at(i);
 
+    // dp.at(i).at(j) := 左端がi、右端がjの状態で得られる最高得点
+    vector<vector<int>> dp(N + 9, vector<int>(N + 9));
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = N; j >= 1; j--)
+        {
 
+            if (i > j)
+                continue;
 
+            if (i < P.at(i) && P.at(i) <= j)
+                dp.at(i + 1).at(j) = max(dp.at(i + 1).at(j), dp.at(i).at(j) + A.at(i));
+            else
+                dp.at(i + 1).at(j) = max(dp.at(i + 1).at(j), dp.at(i).at(j));
 
+            if (i <= P.at(j) && P.at(j) < j)
+                dp.at(i).at(j - 1) = max(dp.at(i).at(j - 1), dp.at(i).at(j) + A.at(j));
+            else
+                dp.at(i).at(j - 1) = max(dp.at(i).at(j - 1), dp.at(i).at(j));
+        }
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= N; i++)
+        ans = max(ans, dp.at(i).at(i));
+    cout << ans << endl;
 }

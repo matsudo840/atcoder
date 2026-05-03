acda1ab7525d13907dbb5be7b913ecffd4848a44
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-05-04 05:16:12 +0900

    繰り返し2乗法

--- tessoku-book/a29/main.cpp
+++ tessoku-book/a29/main.cpp
@@ -5,15 +5,28 @@ using namespace atcoder;
 
 // 型エイリアス（Pythonの int/float の感覚に近づける）
 using ll = long long;
-using mint = modint998244353;
+using mint = modint1000000007;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
 
+ll power(ll a, ll b)
+{
+    ll res = 1;
+    a %= 1000000007; // 必要に応じてMODをとる
+    while (b > 0)
+    {
+        if (b % 2 == 1)
+            res = (res * a) % 1000000007;
+        a = (a * a) % 1000000007;
+        b /= 2;
+    }
+    return res;
+}
+
 int main()
 {
+    ll a, b;
+    cin >> a >> b;
 
-
-
-
-
+    cout << power(a, b) << endl;
 }

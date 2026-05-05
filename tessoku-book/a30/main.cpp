d13dbc1f6f461bf96641e4e8dc55b8ea75052ecb
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-05-05 17:56:55 +0900

    tessoku a30

--- tessoku-book/a30/main.cpp
+++ tessoku-book/a30/main.cpp
@@ -5,15 +5,21 @@ using namespace atcoder;
 
 // 型エイリアス（Pythonの int/float の感覚に近づける）
 using ll = long long;
-using mint = modint998244353;
+using mint = modint1000000007;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
 
 int main()
 {
+    int n, r;
+    cin >> n >> r;
 
+    mint ans = 1;
+    for (int i = n - r + 1; i <= n; i++)
+        ans *= i;
 
+    for (int i = 1; i <= r; i++)
+        ans /= i;
 
-
-
+    cout << ans.val() << endl;
 }

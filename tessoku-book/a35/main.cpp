259f70c0fc30d75abb48621fcf8a18feba8ce2e6
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-05-07 00:45:20 +0900

    tessoku-a35

--- tessoku-book/a35/main.cpp
+++ tessoku-book/a35/main.cpp
@@ -11,9 +11,32 @@ using mint = modint998244353;
 
 int main()
 {
+    int N;
+    cin >> N;
 
+    vector<int> A(N);
+    for (int i = 0; i < N; i++)
+        cin >> A.at(i);
 
+    for (int i = 0; i < N - 1; i++)
+    {
+        bool turn; // true: 太郎の手番, false: 次郎の手番
+        if ((N - i) % 2 == 0)
+            turn = true;
+        else
+            turn = false;
 
+        vector<int> A_tmp;
+        for (int j = 0; j < N - i - 1; j++)
+        {
+            if (turn == true)
+                A_tmp.push_back(max(A.at(j), A.at(j + 1)));
+            else
+                A_tmp.push_back(min(A.at(j), A.at(j + 1)));
+        }
 
+        copy(begin(A_tmp), end(A_tmp), begin(A)); // 配列Aを配列A_tmpで上書き
+    }
 
+    cout << A[0] << endl;
 }

fb983840f6432de9468b03488e610449d75897af
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-05-10 23:43:47 +0900

    tessoku-a37

--- tessoku-book/a37/main.cpp
+++ tessoku-book/a37/main.cpp
@@ -12,8 +12,22 @@ using mint = modint998244353;
 int main()
 {
 
+    ll N, M, B;
+    cin >> N >> M >> B;
 
+    ll A = 0, C = 0;
+    for (int i = 0; i < N; i++)
+    {
+        int a;
+        cin >> a;
+        A += a;
+    }
+    for (int i = 0; i < M; i++)
+    {
+        int c;
+        cin >> c;
+        C += c;
+    }
 
-
-
+    cout << A * M + B * N * M + C * N << endl;
 }

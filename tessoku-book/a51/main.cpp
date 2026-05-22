cde5dd43189d43cc565378ffb1919df252933c96
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-05-22 22:34:41 +0900

    tessoku-a51

--- tessoku-book/a51/main.cpp
+++ tessoku-book/a51/main.cpp
@@ -11,9 +11,25 @@ using mint = modint998244353;
 
 int main()
 {
+    int Q;
+    cin >> Q;
 
+    for (int i = 1; i <= Q; i++)
+    {
+        int q;
+        cin >> q;
 
+        stack<string> S;
 
-
-
+        if (q == 1)
+        {
+            string s;
+            cin >> s;
+            S.push(s);
+        }
+        if (q == 2)
+            cout << S.top() << endl;
+        if (q == 3)
+            S.pop();
+    }
 }

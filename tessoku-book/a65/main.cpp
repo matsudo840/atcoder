b931907e7beb8ed553c2d57eda526720240c4f09
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-06-04 08:31:29 +0900

    tessoku a65

--- tessoku-book/a65/main.cpp
+++ tessoku-book/a65/main.cpp
@@ -9,11 +9,30 @@ using mint = modint998244353;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
 
+int N;
+vector<int> A(100009), dp(100009);
+vector<vector<int>> G(100009);
+
 int main()
 {
+    cin >> N;
+    for (int i = 2; i <= N; i++)
+    {
+        cin >> A.at(i);
+        G.at(A.at(i)).push_back(i);
+    }
 
-
-
-
-
+    for (int i = N; i >= 1; i--)
+    {
+        dp.at(i) = 0;
+        for (int j = 0; j < G.at(i).size(); j++)
+            dp.at(i) += (dp.at(G.at(i).at(j)) + 1);
+    }
+    for (int i = 1; i <= N; i++)
+    {
+        if (i >= 2)
+            cout << " ";
+        cout << dp.at(i);
+    }
+    cout << endl;
 }

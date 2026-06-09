8a551e83abfc716679af1bd6df58adcc124a64d3
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-06-10 03:24:41 +0900

    typ078

--- typical90/078/main.cpp
+++ typical90/078/main.cpp
@@ -9,11 +9,27 @@ using mint = modint998244353;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
 
+int N, M;
+vector<int> G(100009);
+int a, b;
+
 int main()
 {
+    cin >> N >> M;
 
+    for (int i = 0; i < 100009; i++)
+        G.at(i) = 0;
 
+    for (int i = 1; i <= M; i++)
+    {
+        cin >> a >> b;
+        G.at(max(a, b))++;
+    }
 
+    int ans = 0;
+    for (int i = 1; i <= N; i++)
+        if (G.at(i) == 1)
+            ans++;
 
-
+    cout << ans << endl;
 }

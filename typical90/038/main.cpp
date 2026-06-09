7b1e107197c75916837b772a25e86a0904825535
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-06-09 21:33:28 +0900

    typ038

--- typical90/038/main.cpp
+++ typical90/038/main.cpp
@@ -9,11 +9,18 @@ using mint = modint998244353;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
 
+// 10^18
+const long long INF = 1e18;
+
+ll A, B;
+
 int main()
 {
+    cin >> A >> B;
+    ll L = gcd(A, B);
 
-
-
-
-
+    if (A / L > INF / B)
+        cout << "Large" << endl;
+    else
+        cout << L * (A / L) * (B / L) << endl;
 }

2bff9ec34e28ac5483166a83147f88c85b722c13
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-06-14 00:59:23 +0900

    typ022

--- typical90/022/main.cpp
+++ typical90/022/main.cpp
@@ -9,11 +9,17 @@ using mint = modint998244353;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
 
+ll A, B, C;
+
 int main()
 {
+    cin >> A >> B >> C;
+    ll G = gcd(gcd(A, B), C);
 
+    ll ans = 0;
+    ans += A / G - 1;
+    ans += B / G - 1;
+    ans += C / G - 1;
 
-
-
-
+    cout << ans << endl;
 }

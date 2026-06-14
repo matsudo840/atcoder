ed4579de7e86dca32a35789bde39819b05176c07
Ryo Hashiguchi <ryohhhhh@gmail.com>
2026-06-15 00:38:17 +0900

    typ024

--- typical90/024/main.cpp
+++ typical90/024/main.cpp
@@ -9,11 +9,24 @@ using mint = modint998244353;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
 
+ll N, K;
+vector<int> A(1009), B(1009);
+
 int main()
 {
+    cin >> N >> K;
 
+    for (int i = 1; i <= N; i++)
+        cin >> A.at(i);
+    for (int i = 1; i <= N; i++)
+        cin >> B.at(i);
 
+    ll cnt = 0;
+    for (int i = 1; i <= N; i++)
+        cnt += abs(A.at(i) - B.at(i));
 
-
-
+    if (K >= cnt && (K - cnt) % 2 == 0)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 }
